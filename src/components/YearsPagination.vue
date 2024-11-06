aba103f006019f337c03a6e143a6d8684cd3d131
Andrii Kulyk <andrij.kulyk@gmail.com>
2024-11-06 16:29:31 +0200

    add yearsPagination for countryView

---
+++ src/components/YearsPagination.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps({
+  years: {
+    type: Array<number>,
+    required: true,
+  },
+  selectedYear: {
+    type: Number,
+    required: true,
+  },
+  setYear: {
+    type: Function,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <ul class="flex gap-10 w-full justify-center">
+    <li
+      class="border"
+      :class="item === selectedYear ? 'border-red-500' : ''"
+      v-for="(item, i) in years"
+      :key="i"
+    >
+      <button @click="setYear(item)" class="px-5 py-3">
+        {{ item }}
+      </button>
+    </li>
+  </ul>
+</template>
--- src/views/CountryView.vue
+++ src/views/CountryView.vue
@@ -1,9 +1,8 @@
 <script lang="ts">
 import HolidaysList from '@/components/HolidaysList.vue'
+import YearsPagination from '@/components/YearsPagination.vue'
 import api from '@/services/api'
 import type { CountryHoliday, CountryInfo } from '@/services/types'
-
-// const years = [2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030]
 
 export default {
   data() {
@@ -12,10 +11,12 @@ export default {
       countryInfo: {} as CountryInfo,
       holidays: [] as Array<CountryHoliday>,
       selectedYear: 2024,
+      years: [2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030],
     }
   },
   components: {
     HolidaysList,
+    YearsPagination,
   },
   methods: {
     async getStart() {
@@ -24,7 +25,19 @@ export default {
         this.selectedYear,
         this.countryCode as string,
       )
-      console.log(this.holidays)
+    },
+    async setHolidaysByYear() {
+      this.holidays = await api.getCountryYearHolidays(
+        this.selectedYear,
+        this.countryCode as string,
+      )
+    },
+    setYear(year: number) {
+      if (year === this.selectedYear) {
+        return
+      }
+      this.selectedYear = year
+      this.setHolidaysByYear()
     },
   },
   mounted() {
@@ -37,7 +50,8 @@ export default {
   <main class="p-10">
     <section>
       <p class="text-xl font-bold mb-5">{{ countryInfo.commonName }}</p>
-      <HolidaysList :holidays="holidays" />
+      <HolidaysList class="mb-5" :holidays="holidays" />
+      <YearsPagination :years="years" :selectedYear="selectedYear" :setYear="setYear" />
     </section>
   </main>
 </template>
